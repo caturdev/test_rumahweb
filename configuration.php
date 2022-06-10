de6f7da185da15b56a8cef2290c43e4ec43cee22
catur wicaksono <catur.wicaksono.developer@gmail.com>
2022-06-10 15:28:38 +0700

    update

--- configuration.php
+++ configuration.php
@@ -2,6 +2,6 @@
 
 class SConfig
 {
-    var $_site_url = "http://localhost/project/rumahweb/manajemen/";
-    var $_document_root = "C:/xampp/htdocs/project/rumahweb/manajemen";
+    var $_site_url = "";
+    var $_document_root = "";
 }
